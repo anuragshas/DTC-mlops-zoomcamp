1a18a907249ceff630938053130cb666a64d638c
Anurag Singh <anurag.comm@gmail.com>
2022-05-31 15:28:47 +0530

    doen Q5

--- week2/train.py
+++ week2/train.py
@@ -2,9 +2,13 @@ import argparse
 import os
 import pickle
 
+import mlflow
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -15,12 +19,13 @@ def run(data_path):
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
-
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
