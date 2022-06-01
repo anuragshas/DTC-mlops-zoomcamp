8ae1702f8504c78ab78b877d66ec8dcc47c7f99d
Anurag Singh <anurag.comm@gmail.com>
2022-06-02 02:03:25 +0530

    done Q6

--- week2/register_model.py
+++ week2/register_model.py
@@ -65,10 +65,16 @@ def run(data_path, log_top):
 
     # select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
-
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.rmse ASC"]
+    )[0]
+    run_id = best_run.info.run_id
+    model_uri = f"runs:/{run_id}/models"
     # register the best model
-    # mlflow.register_model( ... )
+    mlflow.register_model(model_uri=model_uri, name="nyc-taxi-random-forest-best")
 
 
 if __name__ == '__main__':
